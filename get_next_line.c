5ec3b49106e46164f4ceabc621fe71ab8551f3b8
smetzler <smetzler@smetzlers-MacBook-Air.local>
2021-08-20 00:12:19 +0200

    all

---
+++ get_next_line.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smetzler <smetzler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/16 23:59:22 by smetzler          #+#    #+#             */
+/*   Updated: 2021/08/19 23:57:22 by smetzler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+
+int	ft_get_size(char* line)
+{
+	while (line != '\n' || line != '\0')
+}
+char *get_next_line(int fd)
+{
+	char	*line;
+	static	*save;
+	int		pos;
+	
+	ft_get_size;
+	if (fd < 0 || BUFFER_SIZE <= 0 || !line || !save)
+		return(NULL);
+	return(save);
+}
