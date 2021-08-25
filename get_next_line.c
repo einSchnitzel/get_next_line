131fe3b262495a3fc359aa37fbe377844330133e
smetzler <smetzler@smetzlers-MacBook-Air.local>
2021-08-25 21:41:40 +0200

    get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,25 +6,64 @@
 /*   By: smetzler <smetzler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/16 23:59:22 by smetzler          #+#    #+#             */
-/*   Updated: 2021/08/19 23:57:22 by smetzler         ###   ########.fr       */
+/*   Updated: 2021/08/25 21:37:43 by smetzler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-int	ft_get_size(char* line)
+char	*ft_strdup(const char* s1)
 {
-	while (line != '\n' || line != '\0')
+	char	*thecopy;
+	int		length;
+	int		i;
+
+	i = 0;
+	length = ft_strlen(s1);
+	thecopy = malloc(sizeof(char *) * (length + 1));
+	if (thecopy == NULL)
+		return (NULL);
+	while (i <= length)
+	{
+		thecopy[i] = s1[i];
+		i++;
+	}
+	thecopy[i] = '\0';
+	return (thecopy);
 }
-char *get_next_line(int fd)
+
+char*	ft_get_line(int fd, char* newline, char* address)
 {
-	char	*line;
-	static	*save;
-	int		pos;
+	int		size;
+	char*	helper;
+	size = 1;
 	
-	ft_get_size;
-	if (fd < 0 || BUFFER_SIZE <= 0 || !line || !save)
+	while (address != '\n' || address != '\0')
+	{
+		helper = malloc(size);
+		helper = ft_strdup(newline);
+		free(newline);
+		size++;
+		newline = malloc(size + 1);
+		newline = ft_strdup(helper);
+		newline[size] = read(fd, address, 1);
+		address++;
+		free(helper);
+	}
+	return (newline);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*save;
+	int			pos;
+	
+	line = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return(NULL);
-	return(save);
+	line = ft_get_line(fd, line, save);
+	ft_get_size(line);
+	return(line);
 }
---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smetzler <smetzler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/25 09:42:31 by smetzler          #+#    #+#             */
+/*   Updated: 2021/08/25 09:43:07 by smetzler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4000
+# endif
+
+#endif
